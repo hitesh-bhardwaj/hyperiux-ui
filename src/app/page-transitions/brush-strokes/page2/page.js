7a53d2497da5b06b060c97ef90515cfb3e2fc3f8
Gaurav Verma <briskgaurav@gmail.com>
2026-03-19 18:32:50 +0530

    BrushStrokesTransitiom

---
+++ src/app/page-transitions/brush-strokes/page2/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+   
+            <section className='h-screen w-full bg-zinc-900 flex items-center justify-center'>
+                <p className='text-[6vw] text-center font-medium text-white w-[50%]  tracking-tight leading-[.9]'>BRUSH STROKE ROUTE II</p>
+            </section>
+        </>
+    )
+}
