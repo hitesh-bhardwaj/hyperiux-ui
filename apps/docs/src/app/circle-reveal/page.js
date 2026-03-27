4afd8b2a4b83981697d086db8a7e06e5e92bb062
Ishita Srivastav <ishita.sri001@gmail.com>
2026-03-27 12:04:41 +0530

    circle-reveal-animation

---
+++ apps/docs/src/app/circle-reveal/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CircleReveal from '@/components/TextAnims/CircleReveal'
+
+const page = () => {
+  return (
+    <>
+ <CircleReveal
+      textColor="#111"
+      Text="We craft emotionally intelligent user experiences that are adored globally!"
+      revealText="A visual designer — with skills that haven't been replaced by A.I"
+      circleColorBg="#ff5f00"
+      RevealedTextColor="#ffffff"
+    />
+    </>
+  )
+}
+
+export default page
