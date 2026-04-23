f90f804ead7c94dc0182d80eb665eb838527aa87
Gaurav Verma <briskgaurav@gmail.com>
2026-04-23 10:23:25 +0530

    Pie Chart Loader Structure

---
+++ apps/docs/src/app/page-transitions/piechart/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <section className='h-screen w-full bg-zinc-200 flex items-center justify-center'>
+            <p className='text-[6vw] text-center font-medium text-[#DE4013] w-[50%] tracking-tight leading-[.9]'>PIE CHART PAGE I</p>
+        </section>
+    )
+}
